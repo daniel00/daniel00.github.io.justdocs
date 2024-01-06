e14bbb56103b4ddc554fae5d85b0f730fb12584b
daniel <yookeunsik@naver.com>
2024-01-06 23:13:58 +0900

    add lunr.ko.js

--- index.md
+++ index.md
@@ -3,6 +3,6 @@ title: Home
 layout: home
 ---
 다니엘의 첫 페이지 입니다  
-Hello I am Daniel
+Hello I am Daniel  
 한글 검색 잘 되나요?
 
---
+++ search.js
@@ -0,0 +1,3 @@
+<script src="lunr.js"></script> <!-- lunr.js library -->
+<script src="lunr.stemmer.support.js"></script>
+<script src="lunr.de.js"></script> <!-- or any other language you want -->
